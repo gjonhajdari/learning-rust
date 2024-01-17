0f240e21921c860341a429ee9f0cf266bb74d799
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-17 17:24:35 +0100

    Learned about defining enums

--- 06-enums/6.1-defining-enums/src/main.rs
+++ 06-enums/6.1-defining-enums/src/main.rs
@@ -12,12 +12,38 @@ enum IpAddrKind {
   V6(String),
 }
 
+enum Message {
+  Quit,
+  Move { x: i32, y: i32 },
+  Write(String),
+  ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+  fn call(&self) {
+    println!("Hello!");
+  }
+}
+
 fn main() {
+  // // Version one
   // let four = IpAddrKind::V4(String::from("127.0.0.1"));
   // let loopback = IpAddrKind::V6(String::from("::1"));
-  let four = IpAddrKind::V4(127, 0, 0, 1);
-  let loopback = IpAddrKind::V6(String::from("::1"));
+  
+  // // Version two
+  // let four = IpAddrKind::V4(127, 0, 0, 1);
+  // let loopback = IpAddrKind::V6(String::from("::1"));
 
-  println!("Four: {:?}", four);
-  println!("Loopback: {:?}", loopback);
+  // println!("Four: {:?}", four);
+  // println!("Loopback: {:?}", loopback);
+
+  // let m = Message::Write(String::from("!!!"));
+  // m.call();
+
+  // ---------- Option enum
+  let some_number = Some(5);
+  let some_char = Some('e');
+
+  let absent_number: Option<i32> = None;
+
 }
