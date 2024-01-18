65825179f0bc380b557ae228bde7b2789bce8ee0
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-18 15:08:17 +0100

    Learned about the use keyword

--- 07-module-system/restaurant-example/src/lib.rs
+++ 07-module-system/restaurant-example/src/lib.rs
@@ -45,24 +45,35 @@ mod back_of_house {
   }
 }
 
+use crate::front_of_house::hosting;
+
+// mod customer {
+//   pub fn eat_at_restaurant() {
+//     // Generates an error
+//     hosting::add_to_waitlist();
+//   }
+// }
+
 pub fn eat_at_restaurant() {
-  // ----------
+  // // ----------
   // // Absolute path
   // crate::front_of_house::hosting::add_to_waitlist();
 
   // // Relative path
   // front_of_house::hosting::add_to_waitlist();
-  // ----------
+  // // ----------
 
-  // ----------
+  // // ----------
   // let mut meal = back_of_house::Breakfast::summer("Rye");
   // meal.toast = String::from("Wheat");
   
   // println!("I'd like {} toast please", meal.toast);
-  // ----------
+  // // ----------
 
-  // ----------
-  let order1 = back_of_house::Appetizer::Soup;
-  let order2 = back_of_house::Appetizer::Salad;
-  // ----------
-}
+  // // ----------
+  // let order1 = back_of_house::Appetizer::Soup;
+  // let order2 = back_of_house::Appetizer::Salad;
+  // // ----------
+
+  
+}  
