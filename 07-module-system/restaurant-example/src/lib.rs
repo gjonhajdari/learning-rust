ac900fe80a623f7837e2952e34da3d6f36a35b51
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-18 14:28:23 +0100

    Learned about paths for referencing items in modules

--- 07-module-system/restaurant-example/src/lib.rs
+++ 07-module-system/restaurant-example/src/lib.rs
@@ -1,6 +1,8 @@
+fn deliver_order() {}
+
 mod front_of_house {
-  mod hosting {
-    fn add_to_waitlist() {}
+  pub mod hosting {
+    pub fn add_to_waitlist() {}
 
     fn seat_at_table() {}
   }
@@ -12,4 +14,55 @@ mod front_of_house {
 
     fn take_payment() {}
   }
+}
+
+mod back_of_house {
+  fn fix_incorrect_order() {
+    cook_order();
+    // Look in the parent module. In this case 'crate' root.
+    super::deliver_order();
+  }
+  
+  fn cook_order() {}
+
+  pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+  }
+
+  impl Breakfast {
+    pub fn summer(toast: &str) -> Breakfast {
+      Breakfast {
+        toast: String::from(toast),
+        seasonal_fruit: String::from("peaches"),
+      }
+    }
+  }
+
+  pub enum Appetizer {
+    Soup,
+    Salad,
+  }
+}
+
+pub fn eat_at_restaurant() {
+  // ----------
+  // // Absolute path
+  // crate::front_of_house::hosting::add_to_waitlist();
+
+  // // Relative path
+  // front_of_house::hosting::add_to_waitlist();
+  // ----------
+
+  // ----------
+  // let mut meal = back_of_house::Breakfast::summer("Rye");
+  // meal.toast = String::from("Wheat");
+  
+  // println!("I'd like {} toast please", meal.toast);
+  // ----------
+
+  // ----------
+  let order1 = back_of_house::Appetizer::Soup;
+  let order2 = back_of_house::Appetizer::Salad;
+  // ----------
 }
