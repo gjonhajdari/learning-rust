a324b8cbd8b81a6b68d82478119ad80d359b1d08
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-21 17:50:51 +0100

    Add input validation to show number range

--- 02-guessing-game/src/main.rs
+++ 02-guessing-game/src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-  println!("Guess the number!");
+  println!("Guess the secret number!");
 
   let secret_number = rand::thread_rng().gen_range(1..=100);
 
@@ -16,12 +16,15 @@ fn main() {
     .read_line(&mut guess)
     .expect("Failed to read line!");
     
-    let guess: u32 = match guess.trim().parse() {
+    let guess: i32 = match guess.trim().parse() {
       Ok(num) => num,
       Err(_) => continue,
     };
     
-    println!("You guessed: {guess}");
+    if guess < 1 || guess > 100 {
+      println!("The number is between 1 and 100.");
+      continue;
+    }
     
     match guess.cmp(&secret_number) {
       Ordering::Less => println!("Too small!"),
