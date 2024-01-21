0f9a25392fa4a3945e143e8ba4b50bb9250a06cd
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-21 18:24:46 +0100

    Add Guess struct to handle user input validation

--- 02-guessing-game/src/main.rs
+++ 02-guessing-game/src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+  value: i32,
+}
+
+impl Guess {
+  pub fn new(value: i32) -> Guess {
+    if value < 1 || value > 100 {
+      panic!("Guess value must be between 1 and 100. Value {} given.", {value});
+    }
+
+    Guess { value }
+  }
+
+  pub fn value(&self) -> i32 {
+    self.value
+  }
+}
+
 fn main() {
   println!("Guess the secret number!");
 
@@ -21,12 +39,9 @@ fn main() {
       Err(_) => continue,
     };
     
-    if guess < 1 || guess > 100 {
-      println!("The number is between 1 and 100.");
-      continue;
-    }
+    let guess = Guess::new(guess);
     
-    match guess.cmp(&secret_number) {
+    match guess.value().cmp(&secret_number) {
       Ordering::Less => println!("Too small!"),
       Ordering::Greater => println!("Too big!"),
       Ordering::Equal => {
