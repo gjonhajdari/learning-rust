4d987c41bd948c2caee67e47061242d09482961b
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-28 17:12:01 +0100

    Add search from file functionality to CLI program

--- 12-building-a-cli-program/src/lib.rs
+++ 12-building-a-cli-program/src/lib.rs
@@ -19,10 +19,43 @@ impl Config {
   }
 }
 
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let content = fs::read_to_string(config.file_path)?;
-  
-  println!("With text:\n{content}");
+
+  for line in search(&config.query, &content) {
+    println!("{line}");
+  }
 
   Ok(())
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+  
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+  
+  results
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(search(query, contents), vec!["safe, fast, productive."]);
+  }
 }
