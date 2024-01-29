33fb9fc946d01c9e103f4b20a40b634cb743bfd4
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-01-29 22:27:15 +0100

    Refactor build and search functions to use and accept iterators

--- 12-building-a-cli-program/src/lib.rs
+++ 12-building-a-cli-program/src/lib.rs
@@ -12,14 +12,24 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
+  pub fn build<T>(mut args: T) -> Result<Config, &'static str>
+  where
+    T: Iterator<Item = String>
+  {
+    // Consumes first argument (name of the program)
+    args.next();
     
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    let ignore_case = args.contains(&"--ignore-case".to_string());
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+
+    let file_path = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file path"),
+    };
+    
+    let ignore_case = args.any(|arg| arg == "--ignore-case");
     // let ignore_case = env::var("IGNORE_CASE").is_ok();
     
     Ok(Config {
@@ -49,15 +59,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-  
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-  
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 
@@ -66,15 +71,11 @@ pub fn search_case_insensitive<'a>(
   contents: &'a str,
 ) -> Vec<&'a str> {
   let query = query.to_lowercase();
-  let mut results = Vec::new();
 
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query))
+    .collect()
 }
 
 
