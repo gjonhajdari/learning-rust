850ecdb2155b7cba8294ef600a64ad78e6666e37
Gjon Hajdari <gjonhajdari1@gmail.com>
2024-02-03 21:35:28 +0100

    Learned about the `Drop` trait

---
+++ 15-smart-pointers/15.3-drop-trait/src/main.rs
@@ -0,0 +1,33 @@
+use std::mem::drop;
+
+struct CustomSmartPointer {
+  data: String,
+}
+
+impl Drop for CustomSmartPointer {
+  fn drop(&mut self) {
+    println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+  }
+}
+
+fn main() {
+  // let c = CustomSmartPointer {
+  //   data: String::from("my stuff"),
+  // };
+
+  // let d = CustomSmartPointer {
+  //   data: String::from("other stuff"),
+  // };
+
+  // println!("CustomSmartPointer created.");
+
+  // Forcing values to be dropped with std::mem::drop
+  let c = CustomSmartPointer {
+    data: String::from("some data"),
+  };
+  println!("CustomSmartPointer created.");
+
+  drop(c);
+
+  println!("CustomSmartPointer dropped before the end of main.");
+}
